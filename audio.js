3f6f155a66964a00755ef4acb4050b85d570b6d1
Deborah Aanyu Oduman <deborahtrez12@gmail.com>
2022-10-08 16:25:11 +0300

    Drip irrigation - first commit

--- audio.js
+++ audio.js
@@ -1,53 +1,47 @@
 Amplitude.init({
    "songs":[
       {
-         "name":"Stress Management - Building Awareness",
-         "url":"./audio/Building_Awareness.mp3",
-         "source": "World Health Organization",
-         "source_url": "https://www.who.int/publications/i/item/9789240003927"
+         "name":" What is drip irrigation and why is it important? ",
+         "url":"./audio/What is drip irrigation and why is it important.mp3",
+         "source": "Biovision Infonet",
+         "source_url": "https://infonet-biovision.org/"
       },
       {
-         "name":"Stress Management - Grounding Exercise 1",
-         "url":"./audio/Grounding_Exercise_1.mp3",
-         "source": "World Health Organization",
-         "source_url": "https://www.who.int/publications/i/item/9789240003927"
+         "name":"What are the benefits of a drip irrigation system?",
+         "url":"./audio/What are the benefits of a drip irrigation system.mp3",
+         "source": "Biovision Infonet",
+         "source_url": "https://infonet-biovision.org/"
       },
       {
-         "name":"Stress Management - Grounding Exercise 2",
-         "url":"./audio/Grounding_Exercise_2.mp3",
-         "source": "World Health Organization",
-         "source_url": "https://www.who.int/publications/i/item/9789240003927"
+         "name":"What are the limits of drip irrigation?",
+         "url":"./audio/What are the limits of drip irrigation.mp3",
+         "source": "Biovision Infonet",
+         "source_url": "https://infonet-biovision.org/"
       },
       {
-         "name":"Stress Management - Grounding Exercise 3",
-         "url":"./audio/Grounding_Exercise_3.mp3",
-         "source": "World Health Organization",
-         "source_url": "https://www.who.int/publications/i/item/9789240003927"
+         "name":"What is the cost of a drip irrigation system?",
+         "url":"./audio/What is the cost of a drip irrigation system.mp3",
+         "source": "Biovision Infonet",
+         "source_url": "https://infonet-biovision.org/"
       },
       {
-         "name":"Stress Management - Notice and Name",
-         "url":"./audio/Notice_and_Name.mp3",
-         "source": "World Health Organization",
-         "source_url": "https://www.who.int/publications/i/item/9789240003927"
+         "name":"How can I choose the right drip irrigation system?",
+         "url":"./audio/How can i choose the right drip irrigation system.mp3",
+         "source": "Biovision Infonet",
+         "source_url": "https://infonet-biovision.org/"
       },
       {
-         "name":"Stress Management - Unhooking from Unkind Thoughts",
-         "url":"./audio/Unhooking_from_Unkind_Thoughts.mp3",
-         "source": "World Health Organization",
-         "source_url": "https://www.who.int/publications/i/item/9789240003927"
+         "name":"Why should I choose my drip equipment carefully?",
+         "url":"./audio/Why should i choose my drip equipment carefully.mp3",
+         "source": "Biovision Infonet",
+         "source_url": "https://infonet-biovision.org/"
       },
       {
-         "name":"Stress Management - Making Room",
-         "url":"./audio/Making_Room.mp3",
-         "source": "World Health Organization",
-         "source_url": "https://www.who.int/publications/i/item/9789240003927"
-      },
-      {
-         "name":"Stress Management - Being Kind to Yourself",
-         "url":"./audio/Being_kind_to_yourself.mp3",
-         "source": "World Health Organization",
-         "source_url": "https://www.who.int/publications/i/item/9789240003927"
-      },
-  
+         "name":"How should I maintain a drip irrigation system?",
+         "url":"./audio/How should i maintain a drip irrigation system.mp3",
+         "source": "Biovision Infonet",
+         "source_url": "https://infonet-biovision.org/"
+      }
+
    ]
 });
